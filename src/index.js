3ce1d066ee54502819acdbcfe96753ba29532773
oluwafemioluwatuyi <oluwafemioluwatuyi8@gmail.com>
2022-08-31 12:14:36 +0100

    Robofriends

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+
+
+// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>  
+    <App/>   
   </React.StrictMode>
 );
 
