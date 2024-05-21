fbdd212cb61b88f6cfc26ab8d2f887fcdb178e41
Oluwafemi <olumifemi2016@gmail.com>
2024-05-21 12:30:01 +0100

    converted javascript to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
+import { getData } from './utils/data.utils';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
+
 const App = () => {
   const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
+      const fetchUser = async () =>{
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users');
+      setMonsters(users);
+    };
+    fetchUser();
   }, []);
 
   useEffect(() => {
@@ -22,7 +31,7 @@ const App = () => {
     setFilterMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
--- src/Components/card-list/card-list.component.tsx
+++ src/Components/card-list/card-list.component.tsx
@@ -1,7 +1,11 @@
 import Card from '../card/card.component';
+import { Monster } from '../../App';
 import './card-list.styles.css';
 
-const CardList = ({ monsters }) => (
+type CardListProp = {
+  monsters: Monster[]
+}
+const CardList = ({ monsters }:CardListProp) => (
   <div className='card-list'>
     {monsters.map((monster) => {
       return <Card key={monster.id} monster={monster} />;
