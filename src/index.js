4ceadca41a1b25dc270a3fd287b66d9a49e71d9c
Oluwafemi <olumifemi2016@gmail.com>
2024-05-21 09:41:40 +0100

    added the folder for components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Containers/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
 
-
-
-// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'))
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>  
-    <App/>   
-  </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
